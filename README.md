273e586b0e076e6605d58129480226f6b7fc426f
Trent Bosak <bosaktrent@yahoo.com>
2019-07-10 15:14:19 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Cisco Webex Teams Automation
+This repository contains some simple scripts to automate tasks in Cisco Webex Teams.
+
+To get started you'll need to `install` the required modules.
+
+## webex-create-and-add.py
+This script creates a new Cisco Webex Teams room, adds a user, and sends a message to the new room.
+
+*The details shown below are the default values. Editing them is completely optional.* 
+
+```py
+room_name = "My New Room"               # New room name
+user_name = "gifbot@webex.bot"          # User to add to new room
+message = "Hello World!"                # Message to send to new room
+```
+### Requirements
+* Python 3
+* [Requests][requests_link]
+* Cisco Webex Teams account
+
+*You MUST add your persoanl access token prior to running this script.*
+
+```py
+if __name__ == '__main__':
+    token = "{}"	# Personal access token
+    main()
+```
+
+[requests_link]: https://2.python-requests.org/en/master/
+[webex_link]: https://developer.webex.com/
+
+For information regarding your access token click [here][webex_link] and sign into your Cisco Webex Teams account.
+
+To run this script, run the command: `python3 webex-create-and-add.py`
+
+## Questions/Discussion
+If you find an issue, please raise an issue within the issues section.
+
+Also, if you would like to extend one of the scripts for a more specific use case, please feel free to contact me with any questions/concerns you may have. 
+
+---
+<p>Trent Bosak<br>
+Cisco Technical Sales Engineer Intern</p>
---
+++ webex-create-and-add.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Sample Python script that creats a new room, adds a user, and sends a message.
+
+Copyright (c) 2019 Cisco and/or its affiliates.
+
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.1 (the "License"). You may obtain a copy of the
+License at
+
+               https://developer.cisco.com/docs/licenses
+
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
+
+"""
+__author__ = "Trent Bosak <tbosak@cisco.com>"
+__copyright__ = "Copyright (c) 2019 Cisco and/or its affiliates."
+__license__ = "Cisco Sample Code License, Version 1.1"
+
+import requests
+import json
+
+def createRoom(roomName):
+    # Creates a new room with the name stored in roomName
+    url = "https://api.ciscospark.com/v1/rooms"
+    payload = {
+        'title': roomName
+    }
+    headers = {
+        'Content-Type': "application/x-www-form-urlencoded",
+        'Authorization': token                   # Personal access token
+    }
+    response = requests.request("POST", url, data=payload, headers=headers)
+    return response.json()["id"]
+
+def addUser(userEmail, roomId):
+    # Adds a new user with userEmail to room with roomId
+    url = "https://api.ciscospark.com/v1/memberships"
+    payload = {
+        'roomId': roomId,
+        'personEmail': userEmail
+    }
+    headers = {
+        'Authorization': token,                  # Personal access token
+        'Content-Type': "application/x-www-form-urlencoded",
+    }
+    requests.request("POST", url, data=payload, headers=headers)
+
+def sendMessage(message, roomId):
+    # Sends a message to the new room
+    url = "https://api.ciscospark.com/v1/messages"
+    payload = {
+        'roomId': roomId,
+        'text': message
+    }
+    headers = {
+        'Authorization': token,                  # Personal access token
+        'Content-Type': "application/json",
+    }
+    response = requests.request("POST", url, data=payload, headers=headers)
+    print(response)
+
+def displayDoc():
+    # Displays documentation
+    indent = 4
+    print(
+        __doc__,
+        "Author:",
+        " " * indent + __author__,
+        __copyright__,
+        "Licensed Under: " + __license__,
+        sep="\n"
+        )
+
+def main():
+    room_name = "My New Room"               # New room name
+    user_name = "gifbot@webex.bot"          # User to add to new room
+    message = "Hello World!"                # Message to send to new room
+    roomId = createRoom(room_name)          # Creates a new room and returns roomId
+    addUser(user_name, roomId)              # Adds new user to room
+    sendMessage(message, roomId)            # Sends message to new room
+    displayDoc()
+
+if __name__ == '__main__':
+    token = "{}"                            # Personal access token
+    main()
